3db07c19bae629ac2a95cc076d4ef5dc3cf2505e
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-02-26 23:31:39 -0500

    crud

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,37 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return [1, 2, 3, 4]
 
 def add_element_to_end_of_list(l, element):
-    return None
+    list = [1]
+    list.append(element)
+    return list
 
 def add_element_to_start_of_list(l, element):
-    return None
+    list = [1]
+    list.insert(0, element)
+    return list
 
 def remove_element_from_end_of_list(l):
-    return None
+    list = [1, 2, 3, 4]
+    list.pop()
+    return list
 
 def remove_element_from_start_of_list(l):
-    return None
+    list = [1, 2, 3, 4]
+    del list[0]
+    return list
 
 def retrieve_first_element_from_list(l):
-    return None
+    list = [1, 2, 3, 4]
+    return list[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+    list = [1, 2, 3, 4]
+    return list[index]
 
 def retrieve_last_element_from_list(l):
-    return None
+    list = [1, 2, 3, 4]
+    return list[-1]
